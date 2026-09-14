2e1fed85adbdeb6b5e3c95254f06f7230813a84c
micahlam <micah.h.lam@gmail.com>
2026-09-14 14:54:24 -0400

    new 1

--- main.py
+++ main.py
@@ -32,3 +32,16 @@ hands_module = mp.solutions.hands
 draw = mp.solutions.drawing_utils
 camera = cv2.VideoCapture(0)
 last_change = 0.0
+
+with hands_module.Hands(
+	max_num_hands=1,
+	min_detection_confidence=0.7,
+	min_tracking_confidence=0.7,
+) as hands:
+	while camera.isOpened():
+		success, frame = camera.read()
+		if not success:
+			break
+
+		frame = cv2.flip(frame, 1)
+		result = hands.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
